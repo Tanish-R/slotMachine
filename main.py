cfd40c16e481c792476c077449506a9673a0d7c0
Tanish-R <travinuthala@gmail.com>
2025-08-14 18:22:55 -0400

    Added betting function Users are able to say how much they want to bet per line. The total bet is calculated in the main function.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def deposit():
         amount = int(amount)
 
         if amount <= 0:
-            print("Amount must be greater than 0 \n")
+            print("Amount must be greater than $0 \n")
         else:
             isValidAmount = True
     return amount
@@ -34,13 +34,13 @@ def get_number_of_lines():
 
     while not isValidLines:
         lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES}): ")
-        #checks if lines is a number
+
         if not lines.isdigit():
             print("Please enter an appropriate number \n")
             continue
 
         lines = int(lines)
-        
+
         if lines < 1 or lines > MAX_LINES:
             print(f"Number of lines must be between 1 and {MAX_LINES} \n")
         else:
@@ -48,9 +48,35 @@ def get_number_of_lines():
     return lines
 
 
+'''
+Asks users how much they want to bet
+Continues in a while loop until user enters a number in the given betting range
+'''
+def get_bet():
+    isValidBet = False
+
+    while not isValidBet:
+        print(f"\nValid bets are between ${MIN_BET} and ${MAX_BET} per line.")
+        bet = input(f"How much would you like to bet on each line? $")
+
+        if not bet.isdigit():
+            print("Please enter an appropriate number \n")
+            continue
+
+        bet = int(bet)
+
+        if bet < MIN_BET or bet > MAX_BET:
+            print(f"Bets must be between ${MIN_BET} - ${MAX_BET}\n")
+        else:
+            isValidBet = True
+    return bet
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    bet = get_bet()
+    totalBet = lines * bet
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${totalBet}")
+
 
 main()
