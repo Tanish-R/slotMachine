4c21ba8cb71df5dbd588b165f5a12b2a4ac9f8a3
Tanish-R <travinuthala@gmail.com>
2025-08-14 17:57:28 -0400

    Made function for number of Lines Function checks if user enters a valid number of lines in the given range. Also added a main function so that program can be repeated multiple times.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 '''
 Asks users how much money they want to deposit into their balance
@@ -10,12 +13,44 @@ def deposit():
         amount = input("How much would you like to deposit? $")
         #checks if amount is a number
         if not amount.isdigit():
-            print("Please enter a number \n")
+            print("Please enter an appropriate number \n")
             continue
+
         amount = int(amount)
+
         if amount <= 0:
             print("Amount must be greater than 0 \n")
-        isValidAmount = True
+        else:
+            isValidAmount = True
     return amount
 
-deposit()
+
+'''
+Asks users how many lines they would like to bet on
+Continues in a while loop until user enters a number in the given range
+'''
+def get_number_of_lines():
+    isValidLines = False
+
+    while not isValidLines:
+        lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES}): ")
+        #checks if lines is a number
+        if not lines.isdigit():
+            print("Please enter an appropriate number \n")
+            continue
+
+        lines = int(lines)
+        
+        if lines < 1 or lines > MAX_LINES:
+            print(f"Number of lines must be between 1 and {MAX_LINES} \n")
+        else:
+            isValidLines = True
+    return lines
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
